5085d7f2ca7b73fc5729a1857fa143ddb0ead732
Prince <chimeremezechukwudire@gmail.com>
2020-07-17 20:01:33 +0100

    code clean up

--- index.php
+++ index.php
@@ -3,28 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/style.css">
-    <!-- <link rel="stylesheet" href="css/loader.css"> -->
-    <link rel="icon" type="image/png" href="http://example.com/myicon.png">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"> -->
-    <!-- <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css"> -->
-    <!-- <link rel="stylesheet" href="css/main.css"> -->
-    <!-- <link rel="stylesheet" href="css/normalize.min.css"> -->
+    <link rel="icon" type="image/png" href="./Assets/general/LOGO.png">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <!-- Add the slick-theme.css if you want default styling -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <!-- Add the slick-theme.css if you want default styling -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css">
 
-    <script src="js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-    <!-- Add the slick-theme.css if you want default styling -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
     <!-- <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -80,9 +67,9 @@
                             <p class="mt-3">Say Goodbye to attention difficulty, limited attention and <br> impulsiveness. An app made specially for you. <br> Be the first to know when we launch
                             </p>
                             <div>
-                                <form method="POST" action="./process.php">
+                                <form id='form' method="POST" action="./process.php">
                                     <div class="input-group mt-4">
-                                        <input type="email" class="form-control" placeholder="Enter email address" aria-describedby="basic-addon2" name='email'>
+                                        <input type="email" class="form-control" id='email' placeholder="Enter email address" required aria-describedby="basic-addon2" name='email'>
                                         <div class="input-group-append">
                                             <button class="btn btn-outline" value="submit" type="submit">NOTIFY
                                                 ME</button>
@@ -483,7 +470,8 @@
 
                 // if(valid){
 
-                    let email = $('#email2').val();
+                    let email2 = $('#email2').val();
+                    let email = $('#email').val();
                     e.preventDefault();
 
                     $.ajax({
@@ -491,6 +479,8 @@
                 url: './process.php',
                 data: $(this).serialize(),
                 success: function(data){
+                //     document.querySelector('#email').value = '';
+                //  document.querySelector('#email2').value = '';
                     Swal.fire({
                         'icon': 'success',
                     'title': '',
